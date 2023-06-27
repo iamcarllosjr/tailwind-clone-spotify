aa53a8dcbeb9d4c47a9ab7a4689a764b056a92d8
Carlos <iamcarllosjr@gmail.com>
2023-06-27 18:02:40 -0300

    ajustando responsivo footer

--- src/app/components/Albuns.tsx
+++ src/app/components/Albuns.tsx
@@ -4,8 +4,9 @@ import Album from "../../../public/album.png";
 
 const Albuns = () => {
   return (
-    <div>
-        <a href="" className="group max-h-20 flex items-center gap-4 bg-white/10 rounded-md overflow-hidden hover:bg-white/20 transition-colors">
+    <div className="w-56 md:w-auto">
+        <a href="" className="group max-h-20 flex items-center gap-4 bg-white/10 rounded-md overflow-hidden
+         hover:bg-white/20 transition-colors">
             <Image className="h-full" width={96} src={Album} alt=""></Image>
             <span>Unlimited Love</span>
             <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-4 
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,29 +4,31 @@ import Album from "../../../public/album.png";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center gap-10 md:justify-between bg-zinc-900 border-t border-zinc-700 px-6 py-3">
-        <div className="flex items-center gap-3">
-          <Image src={Album} alt="" width={56} height={56}></Image>
+    <footer className="w-full flex items-center gap-10 justify-between bg-zinc-900 border-t border-zinc-700 px-6 py-3">
+        <div className="w-full ml-5 md:ml-0 flex justify-center gap-3 ">
+          <Image src={Album} alt="" width={56} height={56} className='hidden md:flex'></Image>
         <div className="flex gap-2 md:flex-col items-center md:items-start">
-          <strong className="font-normal">Rope</strong>
-          <span className="text-xs text-xinc-400">Foo Fighters</span>
+          <strong className="font-normal text-[12px] md:text-sm">Rope</strong>
+          <span className=" text-[10px] md:text-xs text-xinc-400">Foo Fighters</span>
         </div>
 
-        <div className="flex md:hidden items-center gap-2">
-            <span className="text-xs text-zinc-400">0:31</span>
-            <div className="h-1 rounded-full w-36 bg-zinc-600">
-              <div className="bg-zinc-200 w-12 h-1 rounded-full"></div>
+        <div className="flex-col md:hidden items-center gap-2">
+            <div className='flex items-center gap-2'>
+               <span className="text-xs text-zinc-400">0:31</span>
+               <div className="h-1 rounded-full w-36 bg-zinc-600">
+                 <div className="bg-zinc-200 w-12 h-1 rounded-full"></div>
+               </div>
+               <span className="text-xs text-zinc-400">2:14</span>
             </div>
-            <span className="text-xs text-zinc-400">2:14</span>
-        <div className="flex md:hidden items-center gap-2">
-          <Heart size={20} />
-          <PlayIcon size={20} />
+            <div className="flex md:hidden items-center justify-center gap-2">
+               <Heart size={20} />
+               <PlayIcon size={20} />
+            </div>
         </div>
-           </div>
         </div>
 
 
-        <div className="flex flex-col items-center gap-2">
+        <div className="w-full flex flex-col items-center gap-2">
            <div className="items-center gap-3 hidden md:flex">
             <Shuffle size={20} className="text-zinc-200"/>
             <SkipBack className="text-zinc-200"/>
@@ -46,7 +48,7 @@ const Footer = () => {
            </div>
         </div>
       
-        <div className="hidden md:flex items-center gap-2">
+        <div className="w-full hidden md:flex items-center gap-2">
           <Mic2 size={20}/>
           <LayoutList size={20}/>
           <Laptop2 size={20}/>
