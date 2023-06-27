ea56acb0f0f02501f72f947a2a77951a6985bf26
Carlos <iamcarllosjr@gmail.com>
2023-06-27 17:07:06 -0300

    ajustando responsivo

--- src/app/components/DailyMix.tsx
+++ src/app/components/DailyMix.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 const DailyMix = () => {
   return (
     <div>
-        <a href="" className="bg-white/5 hover:bg-white/10 p-2 rounded-md flex flex-col gap-2">
+        <a href="" className="w-36 bg-white/5 hover:bg-white/10 p-2 rounded-md flex flex-col gap-2">
               <Image className="w-full" width={120} height={120} src={Album} alt=""></Image>
               <strong>Daily Mix 1</strong>
               <span className="text-xs text-zinc-400">Red Hot Chili Peppers</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import DailyMix from "@/app/components/DailyMix";
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col text-zinc-50">
+    <main className="h-screen flex flex-col text-zinc-50 overflow-x-hidden">
       <div className="flex flex-1">
         <Sidebar />
         <section className="flex-1 bg-zinc-900 p-6">
@@ -17,8 +17,8 @@ export default function Home() {
           </div>
 
           <h1 className="text-2xl mt-10 font-semibold tracking-wide">Good Afternoon</h1>
-
-          <div className="grid grid-cols-2 grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 gap-4 mt-4">
+          <div className="h-[300px] lg:h-auto overflow-auto p-1">
+          <div className="grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 gap-4 mt-4">
           <Albuns/>
           <Albuns/>
           <Albuns/>
@@ -26,15 +26,18 @@ export default function Home() {
           <Albuns/>
           <Albuns/>
           </div>
+          </div>
 
           <h2 className="text-lg mt-10 font-semibold tracking-wide">Made for Carlos Junior</h2>
 
-          <div className="grid grid-cols-3 lg:grid-cols-6 gap-5 mt-4">
+          <div className="w-[550px] lg:w-full overflow-auto p-1">
+          <div className="flex lg:grid lg:grid-cols-6 gap-4 mt-4">
             <DailyMix />
             <DailyMix />
             <DailyMix />
             <DailyMix />
             <DailyMix />
+          </div>
           </div>
         </section>
       </div>
